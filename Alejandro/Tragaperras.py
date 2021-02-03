f920936ac89ab78c2ded8a69fbb29c30fa9dd922
Alejandro Ruiz <alexruizdela@gmail.com>
2021-02-03 19:23:14 +0100

    Tragaperras (funciones, solo texto)

--- Alejandro/Tragaperras.py
+++ Alejandro/Tragaperras.py
@@ -7,11 +7,33 @@
 # Un Black Jack de cartas.
 import random
 
-def pobre(dinero):
+def juego(dinero):
+    if dinero > 0:
+            jugar = str(input("Quieres jugar? "))
+            if jugar == "no":   # Si no quieres jugar dices "no"
+                print("Te has echado atrás y no vas a jugar este juego.")
+                opcion = int(input("Qué opción quieres jugar ahora: "))
+            
+            elif jugar != "no" and dinero > 0: # Si quieres jugar puedes poner cualquier cosa menos "no"
+                dinero -= 1
+                ganar = random.randint(1,80)
+                if ganar <= 2: # Si el random da 1 has ganado el premio
+                    print("Has ganado el grande:" ,grande)
+                    dinero += grande
+                    print("Ahora tienes" ,dinero, "€")
+                
+                else: # Si el random da cualquier otra cosa que no sea 1 imprime esto
+                    print("No has ganado nada, vuelve a intentarlo.")
+                    print("Tienes" ,dinero, "€")
+    return dinero
+
+
+def pobre(dinero, opcion):
     if dinero == 0: # Si no te queda dinero pierdes y te echan del casino
         print("Has perdido todo tu dinero")
         print("Te han hechado del Casino y vas a morir pobre.")
-    return
+        opcion = 0
+    return opcion
     
 
 dinero = 30    # El dinero que tienes
@@ -26,31 +48,37 @@ print("Tienes" ,dinero, "€")
 opcion = 5
 if opcion == 5:
     while opcion == 5: # Mientras la variable tragaperras sea True el bucle se mantiene
-        jugar = str(input("Quieres jugar? ")) # Indicamos cualquier palabra menos "no" para jugar
-        if dinero == 0:
-            print("Has perdido todo tu dinero")
-            print("Te han hechado del Casino y vas a morir pobre.")
-            opcion = 0
-            
-        if jugar == "no":   # Si no quieres jugar dices "no"
-            print("Te has echado atrás y no vas a jugar este juego.")
-            opcion = int(input("Qué opción quieres jugar ahora: "))
-            
-        elif jugar != "no" and dinero > 0: # Si quieres jugar puedes poner cualquier cosa menos "no"
-            dinero -= 1
-            ganar = random.randint(1,90)
-            if ganar <= 2: # Si el random da 1 has ganado el premio
-                print("Has ganado el grande:" ,grande)
-                dinero += grande
-                print("Ahora tienes" ,dinero, "€")
-                
-            else: # Si el random da cualquier otra cosa que no sea 1 imprime esto
-                print("No has ganado nada, vuelve a intentarlo.")
-                print("Tienes" ,dinero, "€")
-            
+        opcion = pobre(dinero, opcion)
+
+        dinero = juego(dinero)
+
+        
+        
+##        if dinero == 0:
+##            print("Has perdido todo tu dinero")
+##            print("Te han hechado del Casino y vas a morir pobre.")
+##            opcion = 0
+##
+##        if dinero > 0:
+##            jugar = str(input("Quieres jugar? ")) # Indicamos cualquier palabra menos "no" para jugar
+##            if jugar == "no":   # Si no quieres jugar dices "no"
+##                print("Te has echado atrás y no vas a jugar este juego.")
+##                opcion = int(input("Qué opción quieres jugar ahora: "))
+##            
+##            elif jugar != "no" and dinero > 0: # Si quieres jugar puedes poner cualquier cosa menos "no"
+##                dinero -= 1
+##                ganar = random.randint(1,80)
+##                if ganar <= 2: # Si el random da 1 has ganado el premio
+##                    print("Has ganado el grande:" ,grande)
+##                    dinero += grande
+##                    print("Ahora tienes" ,dinero, "€")
+##                
+##                else: # Si el random da cualquier otra cosa que no sea 1 imprime esto
+##                    print("No has ganado nada, vuelve a intentarlo.")
+##                    print("Tienes" ,dinero, "€")
 
 
-            
+
         
 
 
